36a30b38eb80a8374865756f7fad32f3d2a07639
Cayke Daniel <contato.cayke@gmail.com>
2023-10-02 02:32:36 -0300

    init project

---
+++ ddl.sql
@@ -0,0 +1,129 @@
+create table "Estabelecimentos" (
+  "id" integer PRIMARY KEY,
+  "cnpj" varchar,
+  "nome" varchar,
+  "data_incricao" timestamp,
+  "tipo_estabelecimento" varchar,
+  "horario_funcionamento" timestamp,
+  "created_at" timestamp,
+  "last_update" timestamp
+);
+
+create table "Enderecos" (
+  "id" integer PRIMARY KEY,
+  "ponto_de_referencia" varchar,
+  "cep" varchar,
+  "descricao_enderco" varchar,
+  "usuario_id" integer,
+  "estabelecimento_id" integer,
+  "pedido_id" integer,
+  "created_at" timestamp,
+  "last_update" timestamp
+);
+
+create table "Usuarios" (
+  "id" integer PRIMARY KEY,
+  "nome" varchar,
+  "email" varchar,
+  "cpf" varchar,
+  "telefone" varchar,
+  "senha" varchar,
+  "created_at" timestamp,
+  "last_update" timestamp
+);
+
+create table "Pedidos" (
+  "id" integer PRIMARY KEY,
+  "estabelecimento_id" integer,
+  "veiculo_id" integer,
+  "usuario_id" integer,
+  "avaliacao_id" integer,
+  "codigo_confirmacao" varchar,
+  "tempo_medio" timestamp,
+  "taxa_entrega" double,
+  "status" varchar,
+  "preco" varchar,
+  "created_at" timestamp,
+  "last_update" timestamp
+);
+
+create table "Produtos" (
+  "id" integer PRIMARY KEY,
+  "nome" varchar,
+  "preco" double,
+  "descricao" varchar,
+  "preco_promocional" double,
+  "created_at" timestamp,
+  "last_update" timestamp
+);
+
+create table "Produtos_Estabelecimentos" (
+  "id" integer PRIMARY KEY,
+  "estabelecimento_id" integer,
+  "produto_id" integer,
+  "pedido_minimo" numeric(10, 2),
+  "status" boolean,
+  "created_at" timestamp,
+  "last_update" timestamp
+);
+
+create table "Pedidos_Produtos" (
+  "id" integer PRIMARY KEY,
+  "produto_id" integer,
+  "pedido_id" integer,
+  "quantidade" integer,
+  "preco_unitario" double,
+  "created_at" timestamp,
+  "last_update" timestamp
+);
+
+create table "Veiculos" (
+  "id" integer PRIMARY KEY,
+  "documentos" varchar,
+  "placa" varchar,
+  "cor" varchar,
+  "entregador_id" integer,
+  "created_at" timestamp,
+  "last_update" timestamp
+);
+
+create table "Entregadores" (
+  "id" integer PRIMARY KEY,
+  "nome" varchar,
+  "cpf" varchar,
+  "created_at" timestamp,
+  "last_update" timestamp
+);
+
+create table "Avaliacoes" (
+  "id" integer PRIMARY KEY,
+  "descricao" varchar,
+  "nota" double,
+  "data" timestamp,
+  "created_at" timestamp,
+  "last_update" timestamp
+);
+
+alter table "Pedidos" ADD FOREIGN KEY ("usuario_id") REFERENCES "Usuarios" ("id");
+
+alter table "Pedidos" ADD FOREIGN KEY ("estabelecimento_id") REFERENCES "Estabelecimentos" ("id");
+
+alter table "Pedidos" ADD FOREIGN KEY ("avaliacao_id") REFERENCES "Avaliacoes" ("id");
+
+alter table "Enderecos" ADD FOREIGN KEY ("estabelecimento_id") REFERENCES "Estabelecimentos" ("id");
+
+alter table "Enderecos" ADD FOREIGN KEY ("usuario_id") REFERENCES "Usuarios" ("id");
+
+alter table "Produtos_Estabelecimentos" ADD FOREIGN KEY ("estabelecimento_id") REFERENCES "Estabelecimentos" ("id");
+
+alter table "Produtos_Estabelecimentos" ADD FOREIGN KEY ("produto_id") REFERENCES "Produtos" ("id");
+
+alter table "Pedidos_Produtos" ADD FOREIGN KEY ("pedido_id") REFERENCES "Pedidos" ("id");
+
+alter table "Pedidos_Produtos" ADD FOREIGN KEY ("produto_id") REFERENCES "Produtos" ("id");
+
+alter table "Veiculos" ADD FOREIGN KEY ("entregador_id") REFERENCES "Entregadores" ("id");
+
+alter table "Pedidos" ADD FOREIGN KEY ("veiculo_id") REFERENCES "Veiculos" ("id");
+
+alter table "Enderecos" ADD FOREIGN KEY ("pedido_id") REFERENCES "Pedidos" ("id");
---
+++ dml.sql
@@ -0,0 +1,80 @@
+INSERT INTO "Estabelecimentos" ("id", "cnpj", "nome", "data_incricao", "tipo_estabelecimento", "horario_funcionamento", "created_at", "last_update")
+VALUES
+  (1, '12345678901234', 'Restaurante A', '2023-10-02 10:00:00', 'Restaurante', '2023-10-02 10:00:00', NOW(), NOW()),
+  (2, '98765432109876', 'Café B', '2023-10-02 08:00:00', 'Café', '2023-10-02 08:00:00', NOW(), NOW()),
+  (3, '56789012345678', 'Pizzaria C', '2023-10-02 18:00:00', 'Pizzaria', '2023-10-02 18:00:00', NOW(), NOW()),
+  (4, '34567890123456', 'Lanchonete D', '2023-10-02 12:30:00', 'Lanchonete', '2023-10-02 12:30:00', NOW(), NOW()),
+  (5, '78901234567890', 'Sorveteria E', '2023-10-02 15:00:00', 'Sorveteria', '2023-10-02 15:00:00', NOW(), NOW());
+
+
+INSERT INTO "Usuarios" ("id", "nome", "email", "cpf", "telefone", "senha", "created_at", "last_update")
+VALUES
+  (1, 'João Silva', 'joao@email.com', '123.456.789-01', '(11) 98765-4321', 'senha123', NOW(), NOW()),
+  (2, 'Maria Santos', 'maria@email.com', '987.654.321-09', '(21) 12345-6789', 'senha456', NOW(), NOW()),
+  (3, 'Pedro Oliveira', 'pedro@email.com', '567.890.123-45', '(31) 23456-7890', 'senha789', NOW(), NOW()),
+  (4, 'Ana Souza', 'ana@email.com', '345.678.901-23', '(41) 34567-8901', 'senha234', NOW(), NOW()),
+  (5, 'Luiz Pereira', 'luiz@email.com', '789.012.345-67', '(51) 45678-9012', 'senha567', NOW(), NOW());
+
+INSERT INTO "Avaliacoes" ("id", "descricao", "nota", "data", "created_at", "last_update")
+VALUES
+  (1, 'Avaliação 1', 4.5, NOW(), NOW(), NOW()),
+  (2, 'Avaliação 2', 3.0, NOW(), NOW(), NOW()),
+  (3, 'Avaliação 3', 5.0, NOW(), NOW(), NOW()),
+  (4, 'Avaliação 4', 4.2, NOW(), NOW(), NOW()),
+  (5, 'Avaliação 5', 4.8, NOW(), NOW(), NOW());
+
+INSERT INTO "Produtos" ("id", "nome", "preco", "descricao", "preco_promocional", "created_at", "last_update")
+VALUES
+  (1, 'Produto 1', 10.99, 'Descrição do Produto 1', 8.99, NOW(), NOW()),
+  (2, 'Produto 2', 5.99, 'Descrição do Produto 2', 4.99, NOW(), NOW()),
+  (3, 'Produto 3', 15.99, 'Descrição do Produto 3', 12.99, NOW(), NOW()),
+  (4, 'Produto 4', 7.99, 'Descrição do Produto 4', 6.99, NOW(), NOW()),
+  (5, 'Produto 5', 9.99, 'Descrição do Produto 5', 7.99, NOW(), NOW());
+
+INSERT INTO "Produtos_Estabelecimentos" ("id", "estabelecimento_id", "produto_id", "pedido_minimo", "status", "created_at", "last_update")
+VALUES
+  (1, 1, 1, 10.00, true, NOW(), NOW()),
+  (2, 2, 2, 5.00, true, NOW(), NOW()),
+  (3, 3, 3, 15.00, true, NOW(), NOW()),
+  (4, 4, 4, 7.00, true, NOW(), NOW()),
+  (5, 5, 5, 9.00, true, NOW(), NOW());
+
+INSERT INTO "Entregadores" ("id", "nome", "cpf", "created_at", "last_update")
+VALUES
+  (1, 'Entregador 1', '11111111111', NOW(), NOW()),
+  (2, 'Entregador 2', '22222222222', NOW(), NOW()),
+  (3, 'Entregador 3', '33333333333', NOW(), NOW()),
+  (4, 'Entregador 4', '44444444444', NOW(), NOW()),
+  (5, 'Entregador 5', '55555555555', NOW(), NOW());
+
+INSERT INTO "Veiculos" ("id", "documentos", "placa", "cor", "entregador_id", "created_at", "last_update")
+VALUES
+  (1, 'Doc1', 'ABC-1234', 'Azul', 1, NOW(), NOW()),
+  (2, 'Doc2', 'DEF-5678', 'Vermelho', 2, NOW(), NOW()),
+  (3, 'Doc3', 'GHI-9012', 'Verde', 3, NOW(), NOW()),
+  (4, 'Doc4', 'JKL-3456', 'Preto', 4, NOW(), NOW()),
+  (5, 'Doc5', 'MNO-7890', 'Branco', 5, NOW(), NOW());
+
+INSERT INTO "Pedidos" ("id", "estabelecimento_id", "veiculo_id", "usuario_id", "avaliacao_id", "codigo_confirmacao", "tempo_medio", "taxa_entrega", "status", "preco", "created_at", "last_update")
+VALUES
+  (1, 1, 1, 1, 1, 'ABCD123', '2023-10-02 11:30:00', 5.00, 'Pendente', '20.00', NOW(), NOW()),
+  (2, 2, 2, 2, 2, 'EFGH456', '2023-10-02 09:45:00', 3.50, 'Entregue', '15.50', NOW(), NOW()),
+  (3, 3, 3, 3, 3, 'IJKL789', '2023-10-02 19:15:00', 4.25, 'Em andamento', '18.25', NOW(), NOW()),
+  (4, 4, 4, 4, 4, 'MNOP123', '2023-10-02 13:20:00', 6.00, 'Entregue', '24.00', NOW(), NOW()),
+  (5, 5, 5, 5, 5, 'QRST567', '2023-10-02 16:30:00', 2.75, 'Pendente', '14.75', NOW(), NOW());
+
+INSERT INTO "Pedidos_Produtos" ("id", "produto_id", "pedido_id", "quantidade", "preco_unitario", "created_at", "last_update")
+VALUES
+  (1, 1, 1, 3, 10.99, NOW(), NOW()),
+  (2, 2, 2, 2, 5.99, NOW(), NOW()),
+  (3, 3, 3, 1, 15.99, NOW(), NOW()),
+  (4, 4, 4, 4, 7.99, NOW(), NOW()),
+  (5, 5, 5, 5, 9.99, NOW(), NOW());
+
+INSERT INTO "Enderecos" ("id", "ponto_de_referencia", "cep", "descricao_enderco", "usuario_id", "estabelecimento_id", "pedido_id", "created_at", "last_update")
+VALUES
+  (1, 'Ponto de Ref 1', '12345-678', 'Endereço 1', 1, 1, 1, NOW(), NOW()),
+  (2, 'Ponto de Ref 2', '23456-789', 'Endereço 2', 2, 2, 2, NOW(), NOW()),
+  (3, 'Ponto de Ref 3', '34567-890', 'Endereço 3', 3, 3, 3, NOW(), NOW()),
+  (4, 'Ponto de Ref 4', '45678-901', 'Endereço 4', 4, 4, 4, NOW(), NOW()),
+  (5, 'Ponto de Ref 5', '56789-012', 'Endereço 5', 5, 5, 5, NOW(), NOW());
